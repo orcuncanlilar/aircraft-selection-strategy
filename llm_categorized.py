452a0cfaefdf55db072e1f83cf3f82a10e7959b5
orcun <orcuncanlilar@gmail.com>
2024-09-27 10:02:39 -0500

    eda

--- llm_categorized.py
+++ llm_categorized.py
@@ -1,35 +1,47 @@
 
-import openai
 import pandas as pd
 
+from langchain_community.chat_models import ChatOllama
 
 df = pd.read_csv('data/report_status.csv')
 
-template = """
-    You are an expert in analyzing aircraft accident reports. 
-    You have been asked to categorize the following aircraft accident reason into one of the following categories: 
-    
-    'Pilot Error', 'Mechanical Failure', 'Weather Conditions', or 'Other'. Provide only the category name as a single word. Reason: {reason}
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+        """
+        You are an expert in analyzing aircraft accident reports. You have been asked to categorize the following aircraft accident reason into one of the following categories: 'Pilot Error', 'Mechanical Failure', 'Weather Conditions', or 'Other'. Provide only the category name as a single word. Reason: {reason}
     
     Example : 
     Reason : Collapse of the landing gear due to worn left main landing gear components.
     
     Category : Mechanical Failure 
     
-    Do not include any additional information or punctuation in your response. An only use the categries provided. 
-    """
+    Do not include any additional information or punctuation in your response. An only use the categries provided. ,
+        """
+        )
+    ]
+)
+
+llm = ChatOllama(
+            model="llama3",
+            temperature=0,
+        )
+
+chain = prompt | llm
+
 
 def get_llm_categorization(reason):
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini", 
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": template}
-        ],
-        max_tokens=5,
-        temperature=0.0,
+    x = chain.invoke(
+    {
+        "reason": reason,
+        
+    }
     )
-    category = response['choices'][0]['message']['content'].strip()
+    
+    category = x.content.strip()
     return category
 
 
